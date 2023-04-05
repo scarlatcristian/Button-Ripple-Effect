d3d30f53ca385dd27e8a266f3280975e54cab9db
cristi <scarlat.cristian.gabriel@gmail.com>
2023-04-05 22:42:08 +0300

    added the CSS and JS

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 "use strict";
+const buttons = document.querySelectorAll(".ripple");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    // Where we click inside the button
+    // console.log(x, y);
+
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+
+    // Where the button is positioned
+    // console.log(buttonTop, buttonLeft);
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    // x and y values from where the button starts
+    // console.log(xInside, yInside);
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
+
+    this.appendChild(circle);
+
+    // Removing the span element
+    setTimeout(() => circle.remove(), 500);
+  });
+});
